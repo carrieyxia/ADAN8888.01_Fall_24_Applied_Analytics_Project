8bac52eb1d701d62d845887362e44e9166c4b719
yeshimonipede <onipede@bc.edu>
2024-09-26 15:36:50 -0400

    Restored deleted notebook files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Project repository for Uber and Lyft Price Prediction, Boston, MA',
+    author='Yu Xia, Hollis Cohan, Yeshim Onipede',
+    license='MIT',
+)
